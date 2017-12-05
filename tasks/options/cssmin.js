dea352faded889ec9cc29ba783ce58af6ac08db3
Mario Aguiar <mario@10up.com>
2017-12-05 17:13:17 -0600

    Add admin css

--- tasks/options/cssmin.js
+++ tasks/options/cssmin.js
@@ -10,7 +10,7 @@ module.exports = {
 		expand: true,
 
 		cwd: 'assets/css/',
-		src: ['mah-downloads.css'],
+		src: ['*.css', '!*.min.css'],
 
 		dest: 'assets/css/',
 		ext: '.min.css'
--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -7,5 +7,15 @@ module.exports = {
 		files: {
 			'assets/css/mah-downloads.css': 'assets/css/sass/mah-downloads.scss'
 		}
+	},
+
+	admin: {
+		options: {
+			precision: 2,
+			sourceMap: true
+		},
+		files: {
+			'assets/css/mah-donwloads-admin.css': 'assets/css/sass/mah-downloads-admin.scss'
+		}
 	}
 };
